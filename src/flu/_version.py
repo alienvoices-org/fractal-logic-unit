39c811d306bb1c1cd68a59110461e838d274f183
AlienVoices <alienvoices@googlemail.com>
2026-04-27 21:46:40 +0200

    Update version to 15.4.0 and build date

--- src/flu/_version.py
+++ src/flu/_version.py
@@ -1,5 +1,5 @@
-__version__       = "15.3.2"
-__build_date__    = "2026-03-27"
+__version__       = "15.4.0"
+__build_date__    = "2026-04-26"
 
 FLU_VERSION       = __version__
 FLU_VERSION_LABEL = f"FLU v{__version__} (build {__build_date__})"
